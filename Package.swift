1d74372de89fbbb4b2a3cb9ece0114b4504a6078
JrFernando10 <fernandojuniior18@gmail.com>
2024-11-02 14:45:59 +0000

    Add Package.swift for DesignSystem

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "DesignSystem",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "DesignSystem",
+            targets: ["DesignSystem"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "DesignSystem",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "DesignSystemTests",
+            dependencies: ["DesignSystem"]
+        ),
+    ]
+)
+
